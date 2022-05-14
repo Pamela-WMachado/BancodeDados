d1147ea09c9545f6222f0b2e7b83f9b03b82e968
Pamela-WMachado <pamela.wehle@gmail.com>
2022-05-14 18:42:41 -0300

    @Pamela-WMachado update id

--- Crud/produtos.js
+++ Crud/produtos.js
@@ -7,10 +7,11 @@ app.use(express.json());
 
 var pg = require('pg');
 
-var conString = 'postgres://bxaybfdyembett:92d19c4cde195202a884492231232a78bc80c99754ba2c359c47fd1a6b4272c1@ec2-44-196-223-128.compute-1.amazonaws.com:5432/dd9f76j2b3hi2m'
-const pool = new pg.Pool({connectionString: conString, ssl: {rejectUnauthorized: false}})
+var conString = 'postgres://kwxxklmjsbpeqs:4411086cc999ba61eb11ec83a22fb515b268a66024f1f4190b0f7b8baf4fccdc@ec2-3-224-164-189.compute-1.amazonaws.com:5432/df0s14c56o9oun';
 
-//rota principal
+const pool = new pg.Pool({connectionString: conString, ssl: {rejectUnauthorized: false}});
+
+//ROTA PRINCIPAL
 app.get('/', (req, res) =>{
     pool.connect((err, product)=>{
         if (err){
@@ -20,30 +21,14 @@ app.get('/', (req, res) =>{
     })
 })
 
-//criação de tabela -> create table produtos (nome varchar(100) , descricao varchar (200), preco varchar (6), foto varchar (400))
-app.get('/criartabelaprodutos', (req, res) =>{
-    pool.connect((err, product)=>{
-        if (err){
-            return res.status(401).send('Não foi possível conectar')
-        }
-            var sql = 'create table produtos (nome varchar (100), descricao varchar (300), preco varchar (6), foto varchar (500))'
-            product.query(sql, (error, result) =>{
-            if (error){
-                return res.status(401).send('Operação não autorizada')
-            }
-            res.status(200).send(result.rows)
-        })
-    })
-})
-
-//cadastrar
+//CADASTRO DE NOVOS PRODUTOS
 app.post('/cadprodutos', (req, res) => {
     pool.connect((err, product) =>{
         if (err) {
             return res.status(401).send('Conexão nao autorizada')
         }
         
-        product.query('select * from produtos where nome = $1', [req.body.nome], (error,result) =>{
+        product.query('select * from produtos where id = $1', [req.body.id], (error,result) =>{
             if (error) {
                 return res.status(401).send('Operação não autorizada')
             }
@@ -52,9 +37,9 @@ app.post('/cadprodutos', (req, res) => {
                 return res.status(200).send('Produto cadastrado')
             }
 
-             var sql = 'INSERT INTO produtos(nome, descricao, preco, foto) VALUES ($1, $2, $3, $4)'
+             var sql = 'INSERT INTO produtos(categoria, preco, foto) VALUES ($1, $2, $3)'
 
-            product.query(sql,[req.body.nome, req.body.descricao, req.body.preco, req.body.foto], (error, result) => {
+            product.query(sql,[req.body.categoria, req.body.preco, req.body.foto], (error, result) => {
                 if (error) {
                     return res.status(403).send('Operação não permitida')
             }
@@ -71,8 +56,8 @@ app.post('/cadprodutos', (req, res) => {
     })
 })
 
-//listando produtos cadastrados
-app.get('/cadprodutos', (req, res) => {
+//LISTANDO PRODUTOS
+app.get('/produtos', (req, res) => {
     pool.connect((err, product) => {
         if(err){
             res.status(401).send('Conexão não autorizada')
@@ -87,13 +72,14 @@ app.get('/cadprodutos', (req, res) => {
     })
 })
 
-//consulta produtos pelo nome
-app.get('/cadprodutos/:nome', (req, res) =>{
+//PESQUISA DE PRODUTO POR ID
+//(tentar add um aviso caso nao exista o id buscado)
+app.get('/produtos/:id', (req, res) =>{
     pool.connect((err, product) => {
         if (err) {
             return res.status(401).send('Conexão não autorizada')
          }
-         product.query('select * from produtos where nome = $1', [req.params.nome], (error, result) =>{
+         product.query('select * from produtos where id = $1', [req.params.id], (error, result) =>{
              if(error) {
                  return res.status(401).send('Operação não autorizada')
              }
@@ -102,14 +88,14 @@ app.get('/cadprodutos/:nome', (req, res) =>{
     })
 })
 
-//metodo deletar
-app.delete('/cadprodutos/:nome', (req, res) =>{
+//DELETAR PRODUTOS
+app.delete('/cadprodutos/:id', (req, res) =>{
     pool.connect((err, product) => {
         if (err) {
            return res.status(401).send('Conexão não autorizada')
         }
 
-        product.query('delete from produtos where nome = $1', [req.params.nome], (error, result) =>{
+        product.query('delete from produtos where id = $1', [req.params.id], (error, result) =>{
 
             if (error) {
                 return res.status(401).send('Operação não autorizada')
@@ -119,21 +105,21 @@ app.delete('/cadprodutos/:nome', (req, res) =>{
     })
 })
 
-//update
-app.put('/cadprodutos/:nome', (req, res) => {
+//UPDATE DE PRODUTOS
+app.put('/cadprodutos/:id', (req, res) => {
     pool.connect((err, product) =>{
         if (err){
             return res.status(401).send('Conexão não autorizada')
         }
-        product.query('select * from produtos where nome=$1', [req.params.nome], (error, result) =>{
+        product.query('select * from produtos where id =$1', [req.params.id], (error, result) =>{
             if (error){
-                return res.status(401).send('Operação não permitida SELECT')
+                return res.status(401).send('Operação não permitida')
             }
 
-            //update produtos set  descricao=$1, preco=$2, foto=$3 where nome=$4
+            //update produtos set  categoria=$1, preco=$2, foto=$3 where id=$4
             if (result.rowCount > 0) {
-                var sql = 'update produtos set descricao=$1, preco=$2, foto=$3 where nome=$4'
-                let valores = [req.body.descricao, req.body.preco, req.body.foto, req.params.nome]
+                var sql = 'update produtos set  categoria=$1, preco=$2, foto=$3 where id=$4'
+                let valores = [req.body.categoria, req.body.preco, req.body.foto, req.params.id]
                 
                 product.query(sql, valores, (error2, result2) =>{
                     if(error2) {
@@ -152,10 +138,4 @@ app.put('/cadprodutos/:nome', (req, res) => {
     })
 })
 
-
-
-
-
-
-
-app.listen(8080, () => console.log('aplicação em execução na url http://localhost:8080'))
+app.listen(8081, () => console.log('aplicação em execução na url http://localhost:8081'));
--- Crud/usuarios.js
+++ Crud/usuarios.js
@@ -2,23 +2,16 @@ const express = require('express');
 const app = express();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken')
+const pg = require('pg');
 
-app.use(express.urlencoded({
-    extended: false
-}));
+
+app.use(express.urlencoded({ extended: false })); 
 app.use(express.json());
 
 
-var pg = require('pg');
+var conString = 'postgres://kwxxklmjsbpeqs:4411086cc999ba61eb11ec83a22fb515b268a66024f1f4190b0f7b8baf4fccdc@ec2-3-224-164-189.compute-1.amazonaws.com:5432/df0s14c56o9oun';
 
-var conString = 'postgres://bxaybfdyembett:92d19c4cde195202a884492231232a78bc80c99754ba2c359c47fd1a6b4272c1@ec2-44-196-223-128.compute-1.amazonaws.com:5432/dd9f76j2b3hi2m'
-
-const pool = new pg.Pool({
-    connectionString: conString,
-    ssl: {
-        rejectUnauthorized: false
-    }
-})
+const pool = new pg.Pool({connectionString: conString, ssl: {rejectUnauthorized: false}});
 
 //rota principal
 app.get('/', (req, res) => {
@@ -27,23 +20,6 @@ app.get('/', (req, res) => {
             return res.status(401).send('Não foi possível conectar')
         }
         res.status(200).send('Conectado com sucesso')
-    })
-})
-
-//criação de tabela -> create table usuarios (nome varchar (30), cpf(11), email varchar(50), senha varchar(200), perfil varchar(15))
-app.get('/criartabelausuarios', (req, res) => {
-    pool.connect((err, client) => {
-        if (err) {
-            return res.status(401).send('Não foi possível conectar')
-        }
-        var sql = 'create table usuarios (nome varchar(30), cpf varchar(11), email varchar(50), senha varchar(200), perfil varchar(15))'
-
-        client.query(sql, (error, result) => {
-            if (error) {
-                return res.status(401).send('Operação não autorizada')
-            }
-            res.status(200).send(result.rows)
-        })
     })
 })
 
@@ -69,15 +45,20 @@ app.post('/cadusuarios', (req, res) => {
                         erro: error.message
                     })
                 }
-                var sql = 'INSERT INTO usuarios(nome, cpf, email, senha, perfil) VALUES ($1, $2, $3, $4, $5)'
-                client.query(sql, [req.body.nome, req.body.cpf, req.body.email, hash, req.body.perfil], (error, result) => {
+                var sql = 'INSERT INTO usuarios (nome, email, senha, cpf, fone, cep, pais, estado, cidade, bairro, rua, numero, complemento, perfil) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)';
+                var valores = [req.body.nome, req.body.email, hash, req.body.cpf, req.body.fone, req.body.cep, req.body.pais, req.body.estado, req.body.cidade, req.body.bairro, req.body.rua, req.body.numero, req.body.complemento, req.body.perfil];
+
+                client.query(sql, valores, (error, result) => {
                     if (error) {
-                        return res.status(403).send('Operação não permitida')
+                        return res.status(403).send(error);
                     }
-                    res.status(201).send({
-                        mensagem: 'Usuário cadastrado com sucesso',
-                        status: 201
-                    })
+                    if (result) {
+                        res.status(201).send({
+                            mensagem: 'Usuário cadastrado com sucesso',
+                            status: 201
+                        })
+                        
+                    }
                 })
             })
         })
@@ -86,29 +67,28 @@ app.post('/cadusuarios', (req, res) => {
 
 
 //listando perfis cadastrados
-app.get('/cadusuarios', (req, res) => {
+app.get('/usuarios', (req, res) => {
     pool.connect((err, client) => {
         if (err) {
-            res.status(401).send('Conexão não autorizada')
+           return res.status(401).send('Conexão não autorizada')
         }
 
         client.query('select * from usuarios', (error, result) => {
             if (error) {
                 return res.status(401).send('Operação não autorizada')
             }
-            res.status(200).send(result.rows)
+             return res.status(200).send(result.rows)
         })
     })
 })
 
-
-//consulta de perfis por cpf
-app.get('/cadusuarios/:cpf', (req, res) => {
+//consulta de perfis por id
+app.get('/usuarios/:id', (req, res) => {
     pool.connect((err, client) => {
         if (err) {
             return res.status(401).send('Conexão não autorizada')
         }
-        client.query('select * from usuarios where cpf = $1', [req.params.cpf], (error, result) => {
+        client.query('select * from usuarios where id = $1', [req.params.id], (error, result) => {
             if (error) {
                 return res.status(401).send('Operação não autorizada')
             }
@@ -117,7 +97,61 @@ app.get('/cadusuarios/:cpf', (req, res) => {
     })
 })
 
-//Login
+
+//update
+app.put('/usuarios/:id', (req, res) => {
+    pool.connect((err, client) => {
+        if (err) {
+            return res.status(401).send('Conexão não autorizada')
+        }
+        client.query('select * from usuarios where id=$1', [req.params.id], (error, result) => {
+            if (error) {
+                return res.status(401).send('Operação não permitida')
+            }
+
+            //update usuarios set nome=$1, email =$2, senha=$3, cpf=$4, fone=$5, cep=$6, pais=$7, estado=$8, cidade=$9, bairro=$10, rua=$11, numero=$12, complemento=$13, perfil=$14
+            if (result.rowCount > 0) {
+                var sql = 'update usuarios set nome=$1, email =$2, senha=$3, cpf=$4, fone=$5, cep=$6, pais=$7, estado=$8, cidade=$9, bairro=$10, rua=$11, numero=$12, complemento=$13, perfil=$14 where id=$15'
+                let valores = [req.body.nome, req.body.email, req.body.senha, req.body.cpf, req.body.fone, req.body.cep, req.body.pais, req.body.estado, req.body.cidade, req.body.bairro, req.body.rua, req.body.numero, req.body.complemento, req.body.perfil, req.params.id];
+
+                client.query(sql, valores, (error2, result2) => {
+                    if (error2) {
+                        return res.status(401).send(error2)
+                    }
+
+                    if (result2.rowCount > 0) {
+                        return res.status(200).send('Dados alterados com sucesso')
+                    }
+                })
+            } else {
+                res.status(200).send('User não encontrado')
+            }
+
+        })
+    })
+})
+
+//metodo deletar
+app.delete('/usuarios/:id', (req, res) => {
+    pool.connect((err, client) => {
+        if (err) {
+            return res.status(401).send('Conexão não autorizada')
+        }
+
+        client.query('delete from usuarios where id = $1', [req.params.id], (error, result) => {
+
+            if (error) {
+                return res.status(401).send('Operação não autorizada')
+            }
+            res.status(200).send({
+                message: 'Usuário excluído com sucesso'
+            })
+        })
+    })
+})
+
+
+//login
 app.post('/usuarios/login', (req, res) => {
     //res.status(200).send('buscar usuário')
     pool.connect((err, client) => {
@@ -158,58 +192,4 @@ app.post('/usuarios/login', (req, res) => {
         })
     })
 })
-
-//metodo deletar
-app.delete('/cadusuarios/:cpf', (req, res) => {
-    pool.connect((err, client) => {
-        if (err) {
-            return res.status(401).send('Conexão não autorizada')
-        }
-
-        client.query('delete from usuarios where cpf = $1', [req.params.cpf], (error, result) => {
-
-            if (error) {
-                return res.status(401).send('Operação não autorizada')
-            }
-            res.status(200).send({
-                message: 'Usuário excluído com sucesso'
-            })
-        })
-    })
-})
-
-//update
-app.put('/cadusuarios/:cpf', (req, res) => {
-    pool.connect((err, client) => {
-        if (err) {
-            return res.status(401).send('Conexão não autorizada')
-        }
-        client.query('select * from usuarios where cpf=$1', [req.params.cpf], (error, result) => {
-            if (error) {
-                return res.status(401).send('Operação não permitida SELECT')
-            }
-
-            //update usuarios set nome=$1, email=$2 senha=$3, perfil=$4 where cpf=$5
-            if (result.rowCount > 0) {
-                var sql = 'update usuarios set nome=$1, email=$2, senha=$3, perfil=$4 where cpf=$5'
-                let valores = [req.body.nome, req.body.email, req.body.senha, req.body.perfil, req.params.cpf]
-
-                client.query(sql, valores, (error2, result2) => {
-                    if (error2) {
-                        return res.status(401).send('Operação não permitida')
-                    }
-
-                    if (result2.rowCount > 0) {
-                        return res.status(200).send('Dados alterados com sucesso')
-                    }
-                })
-            } else {
-                res.status(200).send('User não encontrado')
-            }
-
-        })
-    })
-})
-
-
-app.listen(process.env.PORT || 8080, () => console.log('aplicação em execução na url http://localhost:8080'))
+app.listen(8081, () => console.log('aplicação em execução na url http://localhost:8081'));
